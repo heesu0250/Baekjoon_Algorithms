d71f8f72ca4092a51984602e2f1c081ad32755ad
heesu0250 <heesu0250@naver.com>
2024-10-05 21:37:04 +0900

    [Bronze V] Title: A×B, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int A, B;
+
+    scanf("%d %d", &A, &B); // 사용자에게 두 정수 A, B를 입력받음
+
+    printf("%d\n", A * B); // A와 B의 곱하기 결과 출력
+
+    return 0; // main() 함수의 모든 명령문을 수행한 후 0을 외부로 변환함
+}
