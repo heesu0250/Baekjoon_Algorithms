f94b27d422f4c2316a7a03f61ffccdc2f861c28d
heesu0250 <heesu0250@naver.com>
2024-10-05 10:17:10 +0900

    [Bronze V] Title: A-B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std; // std 네임스페이스를 생략하여 사용
+
+int main() {
+    int A, B; // 두 정수를 저장할 변수 A와 B 선언
+
+    // 사용자로부터 두 정수를 입력받음
+    cin >> A >> B;
+
+    // 두 정수의 차를 출력
+    cout << A - B << endl;
+
+    return 0; // 프로그램 정상 종료
+}
