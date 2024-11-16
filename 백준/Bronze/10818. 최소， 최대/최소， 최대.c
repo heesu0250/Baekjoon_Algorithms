7a7320c19a3ca134b488f6a7b81080f3c0f18ded
heesu0250 <heesu0250@naver.com>
2024-11-16 15:04:42 +0900

    [Bronze III] Title: 최소, 최대, Time: 168 ms, Memory: 4892 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.c
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
-#include <stdlib.h>
 int main(void) {
     int n,i;
     int max_n=-1000000, min_n=1000000;
     scanf("%d", &n);
-    int* num =(int*)malloc(sizeof(int) * n);
+    int num[n];
     for (i = 0; i < n; i++) {
 		scanf("%d", &num[i]);
-        if (num[i] <= min_n)
+        if (num[i] < min_n)
             min_n = num[i];
-        if (num[i] >= max_n)
+        if (num[i] > max_n)
             max_n = num[i];
     }
     printf("%d %d", min_n, max_n);
-    free(num);
     return 0;
 }
